b207285c305d745bf750d86717e75af4a1692142
TusharTalmale <123621060+TusharTalmale@users.noreply.github.com>
2024-05-14 01:37:14 +0530

    Add files via upload

---
+++ Admin/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from '../src/Components/Navbar/Navbar';
+import Admin from "./Pages/Admin/Admin";
+
+const App = ()=>{
+  return (
+    <div>
+      <Navbar/>
+      <Admin/>
+    </div>
+  )
+}
+
+export default App
+//day 4 7.23.37
